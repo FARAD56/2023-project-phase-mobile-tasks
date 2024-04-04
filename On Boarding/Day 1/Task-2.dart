3b9feea1a6c53207150f27454a063d3e21645b1f
Farad Wells <alwaasiua@gmail.com>
2024-04-04 11:13:14 +0000

    removing comments

--- On Boarding/Day 1/Task-2.dart
+++ On Boarding/Day 1/Task-2.dart
@@ -5,7 +5,6 @@ class Task{
   DateTime? dueDate;
 
   Task(String? title, String? description, String? status, DateTime? dueDate){
-    ///A task constructor
     this.title = title;
     this.description = description;
     this.status = status;
@@ -15,32 +14,26 @@ class Task{
 }
 
 class TaskManager{
-  ///Create a Task-List for the Task Manager
   List<Task>  tasks = [];
 
   void addNewTask(String? title, String? description, String? status, DateTime? dueDate){
-    ///Create new task and add to the Task-list
     Task task = Task(title,description,status,dueDate);
     tasks.add(task);
   }
 
   List<Task> viewAllTasks(){
-    ///Return all Tasks in the Task-list
     return tasks;
   }
 
   List<Task> viewOnlyCompletedTasks(){
-    ///Return only Completed Tasks in the Task-list
     return tasks.where((task) => task.status == 'completed').toList();
   }
 
   List<Task> viewOnlyPendingTasks(){
-    ///Return only pending Tasks in the Task-list
     return tasks.where((task) => task.status == 'pending').toList();
   }
 
   void editTask(int index, String? title, String? description, String? status, DateTime? dueDate){
-    ///Get a Task by index in the Task-list and edit it's properties
     if (index > -1 && index < tasks.length){
       if (title != null) tasks[index].title = title;
       if (description != null)  tasks[index].description = description;
@@ -50,7 +43,6 @@ class TaskManager{
   }
 
   void deleteTask(int index){
-    ///Get a Task by index in the Task-list and remove it from the list
     if (index > -1 && index < tasks.length){
       tasks.removeAt(index);
     }
