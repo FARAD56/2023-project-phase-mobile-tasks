efe5c9f8444e7333ce2d9dbc71a4e37ec8fbbbcd
Farad Wells <alwaasiua@gmail.com>
2024-04-04 15:57:21 +0000

    new

--- On Boarding/Day 1/Task-2.dart
+++ On Boarding/Day 1/Task-2.dart
@@ -1,14 +1,53 @@
 class Task{
-  String? title;
-  String? description;
-  String? status;
-  DateTime? dueDate;
+  String? _title;
+  String? _description;
+  String? _status;
+  DateTime? _dueDate;
 
   Task(String? title, String? description, String? status, DateTime? dueDate){
-    this.title = title;
-    this.description = description;
-    this.status = status;
-    this.dueDate = dueDate;
+    this._title = title;
+    this._description = description;
+    this._status = status;
+    this._dueDate = dueDate;
+  }
+
+  void display() {
+    print('Title:   ' + this._title);
+    print('Description:   ' + this._description);
+    print('Due date:   ' + this._dueDate);
+    //print('Status:   '+this.status);
+  }
+
+  void setTitle(String title) {
+    this._title = title;
+  }
+
+  void setDescription(String description) {
+    this._description = description;
+  }
+
+  void setDate(DateTime dueDate) {
+    this._dueDate = dueDate;
+  }
+
+  void setStatus(String? status) {
+    this._status = status;
+  }
+
+  String? getTitle() {
+    return this._title;
+  }
+
+  String? getDescription() {
+    return this._description;
+  }
+
+  DateTime? getDate() {
+    return this._dueDate;
+  }
+
+  String? getStatus() {
+    return this._status;
   }
 
 }
