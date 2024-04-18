c5a20cf4e46b22f4eebe6512639ee8485e91dd20
Farad Wells <alwaasiua@gmail.com>
2024-04-18 10:40:03 +0000

    Remote data source

--- On Boarding/todolist/lib/features/todo/data/datasources/task_remote_data_source.dart
+++ On Boarding/todolist/lib/features/todo/data/datasources/task_remote_data_source.dart
@@ -1,7 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:todolist/core/error/failures.dart';
 import 'package:todolist/features/todo/data/models/todo_model.dart';
 
-abstract class TaskRemoteDataSource{
-  Future<String> createTask(String title, String description, String deadline);
-  Future<List> viewAllTasks();
+abstract class TaskRemoteDataSource {
   Future<TodoModel> viewTask(int id);
+}
+
+class TaskRemoteDataSourceImpl implements TaskRemoteDataSource {
+  final http.Client client;
+
+  TaskRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<TodoModel> viewTask(int id) async {
+    final response = await client.get(Uri.parse('::Not valid URI::'), headers: {
+      'Content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      return TodoModel.fromJson(json.decode(response.body));
+    } else {
+      throw const ServerException(message: 'Something went wrong, 404');
+    }
+  }
 }
